420a78f51fa5cb7885f571408d79fefb78b77be9
Maximilian Remming <maximilian.remming@cs.helsinki.fi>
2015-04-10 02:35:45 +0300

    Small setup changes to main file, added stuff to gitignore

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from flask import Flask, session, redirect, url_for, render_template, request, g
 from flask.ext.bcrypt import Bcrypt
 import sqlite3
 
-# config
-DATABASE = 'data.db'
-DEBUG = False
-SECRET_KEY = 'aesvujzdloaeorvjhnaruoh'
+import sys
 
-# setup
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config.update(
+    DATABASE=sys.argv[1],
+    SECRET_KEY='aufgaofyawgfcjhavywefvakbfvaywe283rtfwegydbha',
+    HOST='0.0.0.0',
+    PORT=5000
+)
 
 bcrypt = Bcrypt(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -58,6 +60,7 @@ def index():
         shouts.insert(0, (shout[0], shout[1], shout[2]))
     return render_template('index.html', shouts=shouts)
 
+
 @app.route('/chat_log')
 @app.route('/chat_log/<int:amount>')
 def chat_log(amount=50):
@@ -68,6 +71,7 @@ def chat_log(amount=50):
         return render_template('chat_log.html', shouts=shouts, amount=amount)
     return redirect(url_for('index'))
 
+
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)
@@ -75,6 +79,7 @@ def logout():
     session.pop('admin', None)
     session.pop('logged_in', None)
     return redirect(url_for('index'))
+
 
 @app.route('/settings', methods=['GET', 'POST'])
 def settings():
@@ -100,8 +105,10 @@ def settings():
 
     return render_template('settings.html')
 
+
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -109,11 +116,13 @@ def get_db():
         db = g._database = connect_db()
     return db
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
 
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
@@ -121,16 +130,22 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+
 def update_db(query, args=()):
     get_db().cursor().execute(query, args)
     get_db().commit()
+
 
 def check_password(user_id, password):
     user = query_db('select name, password from users where user_id = ?', [user_id], one=True)
     return bcrypt.check_password_hash(user[1], user[0] + password)
 
+
 def generate_password_hash(name, password):
     return bcrypt.generate_password_hash(name + password)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    if len(sys.argv) > 2 and sys.argv[2] == 'debug':
+        app.run(debug=True, host='127.0.0.1', port=5000)
+    else:
+        app.run(debug=False, host=app.config['HOST'], port=app.config['PORT'])

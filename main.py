4e1ab51b688ecfea3f7e17cd81405e5b17eefa78
Maximilian Remming <maximilian.remming@cs.helsinki.fi>
2015-04-10 04:54:16 +0300

    Formatting

--- main.py
+++ main.py
@@ -54,26 +54,31 @@ def login():
             flash('Name or password wrong.')
     return render_template('login.html')
 
+
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
     if request.method == 'POST' and session.get('logged_in', False) and request.form['type'] == 'shout':
-        update_db('insert into shouts(user_id, shout, post_time) values (?, ?, datetime(\'now\', \'localtime\'))', [session['user_id'], request.form['content']])
+        update_db('insert into shouts(user_id, shout, post_time) values (?, ?, datetime(\'now\', \'localtime\'))',
+                  [session['user_id'], request.form['content']])
         return redirect(url_for('chat'))
 
     if session.get('logged_in', False):
         shouts = []
-        for shout in query_db('select name, shout, strftime(\'%Y-%m-%d %H:%M\', post_time) from users natural join shouts order by shout_id desc limit 15'):
+        for shout in query_db('select name, shout, strftime(\'%Y-%m-%d %H:%M\', post_time) from users'
+                              ' natural join shouts order by shout_id desc limit 15'):
             shouts.insert(0, (shout[0], shout[1], shout[2]))
         return render_template('chat.html', shouts=shouts)
 
     return redirect(url_for('index'))
+
 
 @app.route('/chat_log')
 @app.route('/chat_log/<int:amount>')
 def chat_log(amount=50):
     if session.get('logged_in', False):
         shouts = []
-        for shout in query_db('select name, shout, strftime(\'%Y-%m-%d %H:%M\', post_time) from users natural join shouts limit ?', [amount]):
+        for shout in query_db('select name, shout, strftime(\'%Y-%m-%d %H:%M\', post_time) from users'
+                              ' natural join shouts limit ?', [amount]):
             shouts.insert(0, (shout[0], shout[1], shout[2]))
         return render_template('chat_log.html', shouts=shouts, amount=amount)
 
@@ -108,7 +113,11 @@ def settings():
 
         if session.get('logged_in', False):
             if request.form['type'] == 'change_password':
-                if not change_password(session.get('name', ''), session.get('user_id', ''), request.form['old_password'], request.form['new_password'], request.form['new_password_again']):
+                if not change_password(session.get('name', ''),
+                                       session.get('user_id', ''),
+                                       request.form['old_password'],
+                                       request.form['new_password'],
+                                       request.form['new_password_again']):
                     flash('Something went wrong, password was not changed.')
                 else:
                     flash('Password changed.')
@@ -118,6 +127,7 @@ def settings():
         return render_template('settings.html')
 
     return redirect(url_for('index'))
+
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
@@ -150,15 +160,18 @@ def update_db(query, args=()):
 
 
 def new_user(name, password, admin=False):
-    if len(name) < 3 or len(password) < 8 or query_db('select count(*) from users where name = ?', [name], one=True)[0] > 0:
+    if len(name) < 3 or len(password) < 8 or \
+                    query_db('select count(*) from users where name = ?', [name], one=True)[0] > 0:
         return False
-    update_db('insert into users(name, password, admin) values (?, ?, ?)', [name, bcrypt.generate_password_hash(name + password), admin])
+    update_db('insert into users(name, password, admin) values (?, ?, ?)',
+              [name, bcrypt.generate_password_hash(name + password), admin])
     return True
 
 
 def change_password(name, user_id, old_password, new_password, new_password_again):
     if new_password == new_password_again and check_password(user_id, old_password):
-        update_db('update users set password = ? where user_id = ?', [generate_password_hash(name, new_password), user_id])
+        update_db('update users set password = ? where user_id = ?',
+                  [generate_password_hash(name, new_password), user_id])
         return True
     return False
 
